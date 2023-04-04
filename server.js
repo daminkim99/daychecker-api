494984c3c04d36b4536cbbbcfc7f592ff1c361a3
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-04-03 23:53:22 -0300

    edit so button works

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const PORT = 8000
+
+app.use(cors())
+
+//middleware that'll deal with static templates 
+app.use("/static", express.static('./static/'))
+//apis
+const days = {
+    'monday': {
+        'mood':'bad'
+    },
+    'tuesday': {
+        'mood': 'better'
+    },
+    'wednesday': {
+        'mood': 'okay'
+    },
+    'thursday': {
+        'mood': 'almost there'
+    },
+    'friday': {
+        'mood': 'TGIFFFFFF'
+    },
+    'saturday': {
+        'mood': 'TGISSSSSS'
+    },
+    'sunday': {
+        'mood': 'RGH monday not again'
+    },
+    'unknown': {
+        'mood': 'must input a day'
+    }
+
+}
+
+app.get('/', (req,res) => {
+    res.sendFile( __dirname +'/index.html')
+})
+
+app.get('/api', (req, res) => {
+    res.json(days)
+})
+//returns to the main.js with the corresponding value from the api request on main.js
+app.get('/api/:day', (req, res) => {
+    const day = req.params.day.toLowerCase()
+    if (days[day]){
+        res.json(days[day]['mood'])
+    } else {
+        res.json(days['unknown']['mood'])
+    }
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+}) 
---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+//adds event to the button so that function is run upon clicking 
+document.querySelector('.check').addEventListener('click', makeReq)
+
+async function makeReq(){
+   const day = document.querySelector('input').value
+   // console.log(day)
+   try{
+    const res = await fetch(`/api/${day}`)
+    const data = await res.json()
+    document.querySelector('#answer').innerText = `Mood: ${data}`
+    
+   } catch(error){
+    console.log(error)
+   }
+}
+
