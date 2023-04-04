9abc73d3c51a1fdc8ddcbd485136fda2154cbff7
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-04-04 00:14:23 -0300

    edit url

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,7 @@ async function makeReq(){
    const day = document.querySelector('input').value
    // console.log(day)
    try{
-    const res = await fetch(`/api/${day}`)
+    const res = await fetch(`https://muddy-cod-handbag.cyclic.app/api/${day}`)
     const data = await res.json()
     document.querySelector('#answer').innerText = `Mood: ${data}`
     
